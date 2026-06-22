f9052eb4f0ff614aa6c5f32a1d726f2ba1c43000
mmyersbyte <pedrovictorcastrodesousa@gmail.com>
2026-06-22 18:15:21 -0300

    refactor: rotas de chat para usar validação de body

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -6,12 +6,14 @@ import {
 } from '../controllers/chatController.js';
 
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { validateBody } from '../middlewares/validateMiddleware.js';
+import { sendMessageSchema } from '../validations/chatValidation.js';
 
 const chatRoutes = Router();
 
 chatRoutes.use(authMiddleware);
 
-chatRoutes.post('/message', sendMessage);
+chatRoutes.post('/message', validateBody(sendMessageSchema), sendMessage);
 chatRoutes.get('/conversations', listConversations);
 
 export default chatRoutes;
