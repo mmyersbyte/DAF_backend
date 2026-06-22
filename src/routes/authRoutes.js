6b9808f330010a0f7b877d43179aa8a507ac903c
mmyersbyte <pedrovictorcastrodesousa@gmail.com>
2026-06-22 18:14:34 -0300

    refactor: rotas de autenticação para usar validação de body

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+
 import { register, login } from '../controllers/authController.js';
+import { validateBody } from '../middlewares/validateMiddleware.js';
+import { registerSchema, loginSchema } from '../validations/authValidation.js';
 
 const authRoutes = Router();
 
-authRoutes.post('/register', register);
-authRoutes.post('/login', login);
+authRoutes.post('/register', validateBody(registerSchema), register);
+authRoutes.post('/login', validateBody(loginSchema), login);
 
 export default authRoutes;
